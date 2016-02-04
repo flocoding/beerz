a66dbd1e35f34a6222e1465d6e6c374ba0abc004
Tjin Au Yeung <tjinauyeung@Tjins-MacBook-Pro.local>
2016-02-04 16:46:05 +0100

    last version

--- beer.rb
+++ beer.rb
@@ -1,36 +1,69 @@
 # just wondering if it works?
-require './Inventorybeer'
-class Beer < Inventorybeer
-  def describe
-    puts "#{@brand} are great!"
-  end
-  def how_many_types_of_beers_are_still_available quantity
-    if  @quantity < quantity
-      puts "Sorry, those just sold-out recently."
-    else
-      puts "We think you will enjoy it!"
-      sleep 3
-    end
+$catalogue = []
+
+class Inventorybeer
+  attr_accessor :name
+  attr_accessor :country
+  attr_accessor :price
+  attr_accessor :quantity
+
+  # def initialize(name, model, price, quantity)
+  #   @name = name
+  #   @model = country
+  #   @price = price
+  #   @quantity = quantity
+  # end
+
+  def add(beer)
+  	$catalogue << beer
   end
 end
 
-Duvel = Beer.new("Duvel", "Belgium", "3.00", "50")
-Guiness = Beer.new("Guinnes", "Ireland", "4.00", "50")
-Amstel= Beer.new("Amstel", "Netherlands", "2.00", "50")
+class Beer 
+	attr_accessor :name, :country, :price, :quantity
+  def initialize(name, country, price, quantity)
+    @name = name
+    @country = country
+    @price = price
+    @quantity = quantity
+  end
+  def name 
+  	return @name
+  end
+  # def describe
+  #   puts "#{@brand} are great!"
+  # end
+  # def how_many_types_of_beers_are_still_available quantity
+  #   if  @quantity < quantity
+  #     puts "Sorry, those just sold-out recently."
+  #   else
+  #     puts "We think you will enjoy it!"
+  #     sleep 3
+  #   end
+  # end
+end
 
+duvel = Beer.new("Duvel", "Belgium", "3.00", "50")
+guiness = Beer.new("Guinnes", "Ireland", "4.00", "50")
+amstel= Beer.new("Amstel", "Netherlands", "2.00", "50")
 
+barbukowski = Inventorybeer.new
 
-puts "what kind of beer do you like (Duvel/Guinnes/Amstel)?"
-brand = gets.chomp.to_i
+barbukowski.add(duvel)
+barbukowski.add(guiness)
+barbukowski.add(amstel)
+
+puts "what kind of beer do you like ( 1: Duvel/ 2: Guinnes/ 3: Amstel)?"
+product_id = gets.chomp.to_i
 
 puts "The bar tender is serving you a ..."
-puts brands[brand].name
+puts $catalogue[product_id-1].name
 sleep 3
 puts "It's fresh from..."
-puts brands[brand].country
+puts $catalogue[product_id-1].country
 puts "We have plenty on stock if you want more later"
-puts brands[brand].quantity
+puts $catalogue[product_id-1].quantity
 puts "The price for this one is"
-puts brands[brand].price
+puts $catalogue[product_id-1].price
 puts "EUR"
-brands[brand].how_many_pairs_are_still_available 2
+# brands[brand].how_many_pairs_are_still_available 2
